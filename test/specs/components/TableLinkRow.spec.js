1ecdc5b421ff17cd4505a404b81745807323490b
Steve Ops <steve.opiyo5@gmail.com>
2018-09-30 00:43:00 +0300

    100% test coverage

--- test/specs/components/TableLinkRow.spec.js
+++ test/specs/components/TableLinkRow.spec.js
@@ -37,4 +37,36 @@ describe('TableLinkRow.vue', () => {
     expect(table.vm.firstField).toMatchObject({type: 'text'})
     expect(table.vm.rest[0]).toMatchObject({type: 'number'})
   })
+  it('gets style', () => {
+    let localVue = createLocalVue()
+    let table = shallowMount(LinkedRow, {
+      localVue,
+      stubs: ['router-link'],
+      propsData: {
+        data: {name: 'me'},
+        fields: [
+          {field: 'image'},
+          {displayRightOnMobile: true},
+        ],
+        styler: () => {},
+        linking: {
+          enabled: true,
+          param: 'name',
+          field: 'name',
+          route: {
+            name: 'route',
+          }
+        }
+      }
+    })
+
+    let field = {styledBackground: {enabled: true, config: {Name: 'Name'}}, field: 'user.name'}
+    let data = {user: {name: 'Name'}}
+
+    expect(table.vm.getStyle(field, data)).toBe('background-color: Name')
+    expect(table.vm.getStyle(field)).toBe('background-color: undefined')
+
+    field = {styledBackground: {enabled: false, config: {Name: 'Name'}}, field: 'user.name'}
+    expect(table.vm.getStyle(field)).toBe('')
+  })
 })
