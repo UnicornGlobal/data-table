9f6ccaf91890208d885ed3e07a1cab927cfe65ae
Steve Ops <steve.opiyo5@gmail.com>
2018-10-02 12:00:55 +0300

    Add new line at eof

--- test/specs/components/FilterDropdown.spec.js
+++ test/specs/components/FilterDropdown.spec.js
@@ -2,6 +2,7 @@ import FilterDropdown from '../../../src/components/FilterDropdown.vue'
 import { createLocalVue, shallowMount } from '@vue/test-utils'
 
 describe('FilterDropdown.vue', () => {
+
   it('it is an object', () => {
     expect(typeof FilterDropdown).toBe('object')
   })
@@ -20,23 +21,7 @@ describe('FilterDropdown.vue', () => {
 
     expect(filter.vm.value('value')).toBe('value')
     expect(filter.vm.value({value: 'value1'})).toBe('value1')
-
     expect(filter.vm.label('label')).toBe('label')
     expect(filter.vm.label({label: 'label1'})).toBe('label1')
   })
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
