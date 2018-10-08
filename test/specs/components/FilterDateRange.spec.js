c541aa75286571fef29d8746ad10a5ffbb10e0c5
Igor <igorsergiichuk@gmail.com>
2018-10-08 20:04:44 +0300

    tests edited 2

--- test/specs/components/FilterDateRange.spec.js
+++ test/specs/components/FilterDateRange.spec.js
@@ -21,7 +21,7 @@ describe('FilterDateRange.vue', () => {
     filter.vm.setDateFrom({date: '12-12-1900'})
     expect(filter.vm.$refs.from.value).toBe('12-12-1900')
 
-    filter.vm.setDateTo({date: '13-12-1900'})
-    expect(filter.vm.$refs.to.value).toBe('13-12-1900')
+    filter.vm.setDateTo({date: '13-12-1901'})
+    expect(filter.vm.$refs.to.value).toBe('13-12-1901')
   })
 })
