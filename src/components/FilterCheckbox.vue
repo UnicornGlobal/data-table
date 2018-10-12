c679866ba793d2c07c9201352cd2729e94169e49
Darryn Ten <darryn.ten@gmail.com>
2018-10-12 13:07:55 +0100

    Themed checkboxes

--- src/components/FilterCheckbox.vue
+++ src/components/FilterCheckbox.vue
@@ -23,6 +23,7 @@
   input[type=checkbox] {
     border: 1px solid var(--primary);
     background-color: white;
+    background-image: none;
   }
 
   input[type=checkbox]:checked {
@@ -45,7 +46,7 @@
     },
     computed: {
       checkboxStyle() {
-        return `background-image: url(data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20viewBox%3D%220%200%20512%20512%22%3E%3Cpath%20d%3D%22M173.898%20439.404l-166.4-166.4c-9.997-9.997-9.997-26.206%200-36.204l36.203-36.204c9.997-9.998%2026.207-9.998%2036.204%200L192%20312.69%20432.095%2072.596c9.997-9.997%2026.207-9.997%2036.204%200l36.203%2036.204c9.997%209.997%209.997%2026.206%200%2036.204l-294.4%20294.401c-9.998%209.997-26.207%209.997-36.204-.001z%22%20fill%3D%22${this.$theme.primary}%22/%3E%3C/svg%3E)`
+        return `background-image: ${this.$assets.checkbox};`
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import DataTable from './DataTable.vue'
 DataTable.install = function(Vue, options) {
   Vue.prototype.$theme = options.theme
 
+  if (!options.theme.primary) {
+    options.theme.primary = '#000'
+  }
+
   Vue.prototype.$assets = {
-    dropdown: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="${escape(options.theme.primary)}" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"/></svg>')`
+    dropdown: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="${encodeURI(options.theme.primary)}" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"/></svg>')`,
+    checkbox: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z" fill="${encodeURI(options.theme.primary)}"/></svg>')`
   }
 
   if (options.assets && options.assets.dropdown) {
