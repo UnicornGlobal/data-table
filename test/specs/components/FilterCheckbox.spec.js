c0d57cc61cae34bf209f389b8a3484ae7b7a8d91
Darryn Ten <darryn.ten@gmail.com>
2018-10-14 22:05:57 +0100

    Update checkbox filter tests

--- test/specs/components/FilterCheckbox.spec.js
+++ test/specs/components/FilterCheckbox.spec.js
@@ -1,5 +1,5 @@
 import FilterCheckbox from '../../../src/components/FilterCheckbox.vue'
-import { createLocalVue, mount } from '@vue/test-utils'
+import { createLocalVue, mount, shallowMount } from '@vue/test-utils'
 
 describe('FilterCheckbox.vue', () => {
   it('it is an object', () => {
@@ -32,5 +32,26 @@ describe('FilterCheckbox.vue', () => {
     let input = filter.find('input')
     expect(input.is('input')).toBe(true)
     expect(input.attributes('id')).toBe('filter-fieldy')
+  })
+
+  it('uses configured checkbox', () => {
+    let localVue = createLocalVue()
+    let filter = shallowMount(FilterCheckbox, {
+      localVue,
+      mocks: {
+        $assets: {
+          checkbox: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"><path stroke="#123" stroke-width="3.4" d="m2.4,2.4 15.2,15.2m0-15.2-15.2,15.2"/></svg>')`
+        }
+      },
+      propsData: {
+        filter: {
+          type: 'checkbox',
+          field: 'fieldy',
+          text: 'text1'
+        }
+      }
+    })
+
+    expect(filter.vm.checkboxStyle).toBe(`background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"><path stroke="#123" stroke-width="3.4" d="m2.4,2.4 15.2,15.2m0-15.2-15.2,15.2"/></svg>');`)
   })
 })
