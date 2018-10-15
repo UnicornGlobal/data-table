890a9a62fa3f12a558f9cb8471bba794f7f56ef5
Darryn Ten <darryn.ten@gmail.com>
2018-10-15 13:12:26 +0100

    Tests for custom field sorting

--- test/specs/components/TableHeaders.spec.js
+++ test/specs/components/TableHeaders.spec.js
@@ -28,4 +28,29 @@ describe('TableHeaders', () => {
     expect(headers.vm.config.sorting.ascending).toBe(false)
     expect(headers.vm.config.sorting.field).toBe('name')
   })
+
+  it('sorts custom fields', () => {
+    let headers = shallowMount(TableHeaders, {
+      localVue,
+      propsData: {
+        config: {
+          sorting: {
+            field: 'index',
+            ascending: true
+          }
+        },
+        fields: [],
+        styler: () => {},
+        controls: []
+      }
+    })
+
+    headers.vm.sort({field: 'name', sort: 'xx'})
+    expect(headers.vm.config.sorting.ascending).toBe(true)
+    expect(headers.vm.config.sorting.field).toBe('xx')
+
+    headers.vm.sort({field: 'name', sort: 'xx'})
+    expect(headers.vm.config.sorting.ascending).toBe(false)
+    expect(headers.vm.config.sorting.field).toBe('xx')
+  })
 })
